39dce2c12664ad33f1daeebb931ee6e2692efcff
Sean O'Connor <oconnorjamessean@gmail.com>
2018-03-05 18:16:00 +0000

    Added activity Indicators for image loading

--- Creative Pod/View/CreativeProductCell.swift
+++ Creative Pod/View/CreativeProductCell.swift
@@ -12,9 +12,33 @@ class CreativeProductCell: UICollectionViewCell {
     @IBOutlet weak var creativeImg: UIImageView!
     @IBOutlet weak var datedLbl: UILabel!
     
+    let activityIndicator = UIActivityIndicatorView()
+    
+    let cachedImage = NSCache<AnyObject, AnyObject>()
+    
     func updateUI(image: Images) {
         
         datedLbl.text = image.dated
+        
+        self.creativeImg.image = nil
+        
+        activityIndicator.color = .blue
+        
+        addSubview(activityIndicator)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        activityIndicator.startAnimating()
+        
+        if let imageIsCached = cachedImage.object(forKey: image.imageID as AnyObject) as? UIImage {
+            
+            self.creativeImg.image = imageIsCached
+            print("Cached Image")
+            activityIndicator.stopAnimating()
+            return
+            
+        }
         
         let imageURLConversion = URL(string: image.imageID)
         URLSession.shared.dataTask(with: imageURLConversion!, completionHandler: { (data, response, error) in
@@ -23,20 +47,20 @@ class CreativeProductCell: UICollectionViewCell {
                 
                 DispatchQueue.main.async {
                     
-                    let downloadedImage = UIImage(data: data!)
+                    if let imageFromFirebase = UIImage(data: data!) {
+                        
+                        self.cachedImage.setObject(imageFromFirebase, forKey: image.imageID as AnyObject)
+                        
+                        self.creativeImg.image = imageFromFirebase
+                        
+                    }
                     
-                    UIGraphicsBeginImageContextWithOptions((downloadedImage?.size)!, false, (downloadedImage?.scale)!)
-                    let rectangle = CGRect(x: 0, y: 0, width: (downloadedImage?.size.width)!, height: (downloadedImage?.size.height)!)
-                    downloadedImage?.draw(in: rectangle)
-                    
-                    let orientatedImage = UIGraphicsGetImageFromCurrentImageContext()
-                    UIGraphicsEndImageContext()
-                    
-                    self.creativeImg.image = orientatedImage
+                    self.activityIndicator.stopAnimating()
                 }
                 
             } else {
                 print(error as Any)
+                self.activityIndicator.stopAnimating()
                 return
             }
             
