3b71dc08c36ed616ee3e0094cb1a97d48740a5a6
Sean O'Connor <oconnorjamessean@gmail.com>
2018-04-28 12:56:41 +0100

    All commenting finalised

--- Creative Pod/Controller/Creative Product Controllers/MembersCreativePieces.swift
+++ Creative Pod/Controller/Creative Product Controllers/MembersCreativePieces.swift
@@ -11,12 +11,18 @@ import FirebaseAuth
 import FirebaseDatabase
 import FirebaseStorage
 
+/*
+ 
+ @brief This class determines behaviour of the MembersCreativePieces.swift interface used by an all Users to view images of members.
+ 
+ */
+
 class MembersCreativePieces: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    //Initialisation of empty variables and components to be used
     
     @IBOutlet weak var membersCollectionView: UICollectionView!
     @IBOutlet weak var membersNameLbl: UILabel!
-    
-    //private var _userName: Users!
     
     private var _groupName: Groups!
     
@@ -33,6 +39,8 @@ class MembersCreativePieces: UIViewController, UICollectionViewDelegate, UIColle
     
     var viewControllerCameFrom: String = ""
     
+    //Initialisation of model objects
+    
     var userName: Users = Users(typeOfUser: "", userName: "", groupName: "")
     
     var storySelection: StorySelection = StorySelection(groupName: "", imageSelected: 0)
@@ -43,6 +51,8 @@ class MembersCreativePieces: UIViewController, UICollectionViewDelegate, UIColle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Depending on what view was previously visited determines the actions that a User is able to take.
         
         if viewControllerCameFrom == "NameList" {
             
@@ -68,6 +78,7 @@ class MembersCreativePieces: UIViewController, UICollectionViewDelegate, UIColle
     }
     
     
+    //Initialises the CollectionView and determines what will be placed within each cell. Takes advantage of CreativeProductCell to determine what does in it.
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CreativeProductCell", for: indexPath) as? CreativeProductCell {
@@ -86,13 +97,16 @@ class MembersCreativePieces: UIViewController, UICollectionViewDelegate, UIColle
         
     }
     
+    //Decides what occurs when a cell is pressed. It is different depending on what view controller user previously came from.
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
+        //If a Buddy they can only simply view the image as a larger piece if selected.
         if viewControllerCameFrom == "NameList" {
         
             let imageSelected = imageArray[indexPath.row]
             performSegue(withIdentifier: "ImageOfMemberViewed", sender: imageSelected)
-            
+          
+        //This is for when creating a story. Depending on the previous UIImage selected from CreationOfStoryboard.swift depends what the ImageSelected object is like.
         } else if viewControllerCameFrom == "CreationOfStoryboard" {
             
             if storySelection.imageSelected == 1 {
@@ -132,6 +146,7 @@ class MembersCreativePieces: UIViewController, UICollectionViewDelegate, UIColle
         
     }
     
+    //Prepares data to be transferred between controllers.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         if let destination = segue.destination as? ImagesViewed {
@@ -143,7 +158,6 @@ class MembersCreativePieces: UIViewController, UICollectionViewDelegate, UIColle
             }
         }
         
-        //TODO:
         if let destination = segue.destination as? CreationOfStoryboard {
             
             if let imageTouched = sender as? ImageSelected {
@@ -157,19 +171,23 @@ class MembersCreativePieces: UIViewController, UICollectionViewDelegate, UIColle
         
     }
     
+    //Decides how many cells there will be depending on images taken
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
         return imageArray.count
         
     }
     
+    //Decides sections
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
+    //Decides size of each collectionView cell.
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        return CGSize(width: 330, height: 330 )
+        return CGSize(width: 400, height: 450 )
         
     }
     
@@ -180,19 +198,21 @@ class MembersCreativePieces: UIViewController, UICollectionViewDelegate, UIColle
         
     }
     
+    //Functions loads all the images to be viewed by for story creation by all Members of a particular group that have given the artist permission.
     
     func loadingAllImages() {
         
+        //Retrieves the current user on the application type.
         let currentUserReference = Database.database().reference().child("users").child((Auth.auth().currentUser?.uid)!)
         
         currentUserReference.observeSingleEvent(of: .value) { (snapshot) in
             
-            //RETRIEVED CURRENT TYPE
             let type = snapshot.value as? [String: AnyObject]
             self.currentUserType = type!["Type"] as! String
             
         }
         
+        //Creates reference to the 'users' branch and observes children for the branch.
         let membersReference = Database.database().reference().child("users")
         
         membersReference.observe(DataEventType.value) { (snapshot) in
@@ -204,19 +224,22 @@ class MembersCreativePieces: UIViewController, UICollectionViewDelegate, UIColle
                     
                     self.memberUID = membersJSON.key
                     
+                    //Extracts group details from every user.
                     let memberElement = membersJSON.value as? [String: AnyObject]
                     self.groupNameStored = memberElement?["Group"] as! String
                     
+                    //If the user's group is the same as the group of the Artist creating a storyboard
                     if self.storySelection.groupName == self.groupNameStored {
                         
+                        //Create a reference to the 'images' branch to those and observes those particular users images in the same group
                         let imageReference = Database.database().reference().child("images").child("\(self.memberUID)")
                         
                         imageReference.observe(DataEventType.value) { (snapshot) in
                             if snapshot.childrenCount > 0 {
                                 
-                                
                                 for imagesJSON in snapshot.children.allObjects as! [DataSnapshot] {
                                     
+                                    //Extract data from the users child.
                                     let imageElement = imagesJSON.value as? [String: AnyObject]
                                     self.dateStored = imageElement?["Date"] as! String
                                     self.keptPrivate = imageElement?["Kept Private"] as! String
@@ -225,6 +248,8 @@ class MembersCreativePieces: UIViewController, UICollectionViewDelegate, UIColle
                                     self.imageURL = imageElement?["imageURL"] as! String
                                     
                                     let adjustedDateString = self.dateStored.replacingOccurrences(of: " +0000", with: "")
+                                    
+                                    //If access has been given to the image and depending on current user than append to the imageModel to be displayed within the collectionview.
                                     
                                     if self.sharedWithBuddy == "Access Granted" && self.currentUserType == "Buddy" {
                                         
@@ -267,20 +292,21 @@ class MembersCreativePieces: UIViewController, UICollectionViewDelegate, UIColle
     }
     
     
-    
+    //Function to loads Members images depending on what user is trying to gain access to them.
     
     func loadingUserImages() {
         
+        //Gauge current user logged into application type.
         let currentUserReference = Database.database().reference().child("users").child((Auth.auth().currentUser?.uid)!)
         
         currentUserReference.observeSingleEvent(of: .value) { (snapshot) in
             
-            //RETRIEVED CURRENT TYPE
             let type = snapshot.value as? [String: AnyObject]
             self.currentUserType = type!["Type"] as! String
             
         }
         
+        //Creation of reference to 'users' branch to observe all child nodes.
         let membersReference = Database.database().reference().child("users")
         
         membersReference.observe(DataEventType.value) { (snapshot) in
@@ -295,14 +321,18 @@ class MembersCreativePieces: UIViewController, UICollectionViewDelegate, UIColle
                     let memberElement = membersJSON.value as? [String: AnyObject]
                     self.memberNameStored = memberElement?["Name"] as! String
                     
+                    //If the selected member to view their images of is the same as a child within the database.
                     if self.userName.userName == self.memberNameStored {
                         
+                        
+                        //Get a reference and observe their values on the 'images' branch for that chosen user.
                         let imageReference = Database.database().reference().child("images").child("\(self.memberUID)")
                         
                         imageReference.observe(DataEventType.value) { (snapshot) in
                             if snapshot.childrenCount > 0 {
+                            
                                 
-                                
+                                //Loop through data.
                                 for imagesJSON in snapshot.children.allObjects as! [DataSnapshot] {
                                     
                                     let imageElement = imagesJSON.value as? [String: AnyObject]
@@ -315,6 +345,7 @@ class MembersCreativePieces: UIViewController, UICollectionViewDelegate, UIColle
                                     let adjustedDateString = self.dateStored.replacingOccurrences(of: " +0000", with: "")
                                     
                                     
+                                    //If user trying to view the Members images have been given permission then they can view the images.
                                     if self.sharedWithBuddy == "Access Granted" && self.currentUserType == "Buddy" {
                                         
                                         let imageModel = Images(imageID: self.imageURL, dated: adjustedDateString, memberName: self.userName.userName)
